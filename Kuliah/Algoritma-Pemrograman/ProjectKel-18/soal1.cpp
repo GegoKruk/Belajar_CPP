4d25a5da72cd057de67d959457cab77c34da93ca
Gega Ramadhan <gegaramadhan17ok@gmail.com>
2025-11-20 19:49:48 +0700

    Menambah isi file soal1 - folder project kel18

--- Kuliah/Algoritma-Pemrograman/ProjectKel-18/soal1.cpp
+++ Kuliah/Algoritma-Pemrograman/ProjectKel-18/soal1.cpp
@@ -14,10 +14,49 @@ using namespace std;
         60 75 85 60 85 64 77 85 76 55
 
         Hasil : 
+
         Nilai tertinggi = 85
         Banyaknya mahasiswa memiliki nilai tertinggi = 2
 */
 
+
+int nilaiUjian[10];
+int nilaiTertinggi = 0;
+int banyakNilaiTertinggi = 0;
+
 int main(){
 
+    cout << "==========[Program Nilai Mahasiswa]==========" << endl;
+
+    //Input nilai
+    cout << "1. Input Nilai Mahasiswa" << endl;
+    int noMhs = 0;
+    while(noMhs < 10){
+        cout << "   - Masukkan Nilai Mahasiswa [" << noMhs + 1 << "]: ";
+        cin >> nilaiUjian[noMhs];
+
+        if(nilaiUjian[noMhs] < 1 || nilaiUjian[noMhs] > 100){//error handling bila input di luar range
+            cout << "\n=====[Input Tidak Valid - Program Selesai]=====" << endl;
+            noMhs = 0;
+            exit(0);
+        } else if(nilaiUjian[noMhs] > nilaiTertinggi){ //Mencari nilai tertinggi
+            nilaiTertinggi = nilaiUjian[noMhs];
+        }
+
+        noMhs++;
+    }
+
+    //Mencari banyak mahasiswa yang meraih nilai tertinggi
+    noMhs = 0;
+    while(noMhs < 10){
+        
+        if(nilaiTertinggi == nilaiUjian[noMhs]){
+            banyakNilaiTertinggi++;
+        }
+
+        noMhs++;
+    }
+    
+    cout << "\n2. Nilai Tertinggi : " << nilaiTertinggi << endl;
+    cout << "3. Banyak Mahasiswa yang Meraih Nilai Tertinggi : " << banyakNilaiTertinggi << endl;
 }
