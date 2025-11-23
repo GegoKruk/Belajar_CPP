04bc231907f5161bde5995d1c322eb814cb43b05
Gega Ramadhan <gegaramadhan17ok@gmail.com>
2025-11-23 17:51:41 +0700

    Menambah isi file soal3 - folder project kel18

--- Kuliah/Algoritma-Pemrograman/ProjectKel-18/soal3.cpp
+++ Kuliah/Algoritma-Pemrograman/ProjectKel-18/soal3.cpp
@@ -17,6 +17,43 @@ using namespace std;
         
 */
 
-int main(){
+float penghitung(){
+    // Data Array Nilai
+    int nilai[10] = {2,5,1,2,4,6,3,2,7,1};
+    float totalNilaiTersaring = 0;
+    int banyakNilaiTersaring = 0;
+    float nilaiRata;
     
+    // Menampilkan Data Nilai
+    cout << "1. Data Nilai: " ;
+    
+    for(int i = 0; i < 10; i++){
+        
+        cout << nilai[i] << " ";
+        
+        // Seleksi mencari data yang < 5 dan >= 7
+        if(nilai[i] < 5 || nilai[i] >= 7){
+            totalNilaiTersaring += nilai[i];
+            banyakNilaiTersaring++;
+        } 
+    }
+
+    nilaiRata = totalNilaiTersaring / banyakNilaiTersaring;
+
+    cout << "\n2. Banyak data yang bernilai < 5 dan >= 7 adalah: " << banyakNilaiTersaring << endl;
+
+    return nilaiRata;
+}
+
+int main(){
+    system("CLS");
+
+    // Judul Program
+    cout << "\n=====[Program Penghitung Nilai Rata-Rata]=====" << endl;
+
+    // Memanggil Function & masuk ke variabel
+    float hasil = penghitung();
+
+    // Menampilkan Hasil Akhir
+    cout << "3. Rata-rata dari nilai yang < 5 dan >= 7 adalah: " << hasil << endl;
 }
