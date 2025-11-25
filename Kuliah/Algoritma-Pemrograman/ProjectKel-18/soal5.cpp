61c9bab31234c442dcf81b405883f231d9a6647e
Gega Ramadhan <gegaramadhan17ok@gmail.com>
2025-11-25 22:02:22 +0700

    Menambah isi file soal5 - folder project kel18

--- Kuliah/Algoritma-Pemrograman/ProjectKel-18/soal5.cpp
+++ Kuliah/Algoritma-Pemrograman/ProjectKel-18/soal5.cpp
@@ -19,6 +19,77 @@ using namespace std;
     }
 */
 
-int main(){
-    
+void banyakPecahan(long uang, long pecahan[]);
+
+void nimNama(){
+    system("CLS");
+
+    cout << "--------------------------------" << endl;
+    cout << "| Nama : Gega Ramadhan         |" << endl;
+    cout << "| NIM  : 123250112             |" << endl;
+    cout << "--------------------------------" << endl;
 }
+
+int main(){ 
+    long uang,pecahan[9] = {50000,20000,10000,5000,2000,1000,500,200,100};
+    int lembar[9];
+
+    // Tampilkan Nama & NIM
+    nimNama();
+
+    // Tampilan Awal Programm
+    cout << "\n=====[Program Penghitung KUang Kembalian]=====" << endl;
+    
+    cout << "Masukkan Jumlah Uang Kembali : Rp "; 
+    cin >> uang; 
+    cout << "Pecahan uang kembali         : " << endl; 
+    
+    banyakPecahan(uang,pecahan);
+}
+
+void banyakPecahan(long uang, long pecahan[]){
+    for (int i = 0; i < 9; i++){
+        int jumlah = uang / pecahan[i];
+        if (jumlah > 0){
+            if (pecahan[i] >= 1000){
+                cout << " - " << jumlah << " Lembar Rp " << pecahan[i] << endl;
+            } else {
+                cout << " - " << jumlah << " Keping Rp " << pecahan[i] << endl;
+            }
+            uang = uang % pecahan[i]; // Update Uang dengan modulus
+        }
+    }
+}
+
+/*
+    void banyakPecahan(long uang, long pecahan[]){
+        for (int i = 0; i < 9; i++){
+            int hasilBagi = uang / pecahan[i];
+
+            if (hasilBagi != 0){
+                cout << hasilBagi << " Lembar Rp " << pecahan[i] << endl;
+
+                for (int j = 0; j < 9; j++){
+                    int uangLebih = uang % pecahan[j];
+
+                    if (uangLebih > 0){
+
+                        for (int k = 0; k < 9; k++){
+                            int banyakUangLebih = uangLebih / pecahan[k];
+                            // 
+                            if (banyakUangLebih > 0){
+                                cout << banyakUangLebih << " Lembar Rp " << pecahan[k] << endl;
+                                break;
+                            } else if (banyakUangLebih > 0 && pecahan[k] < 1000){
+                                cout << banyakUangLebih << " Keping Koin Rp " << pecahan[k] << endl;
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+    }
+*/
