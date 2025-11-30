abf6910bff0c5f3990e83231aab92f6645c53beb
Gega Ramadhan <gegaramadhan17ok@gmail.com>
2025-11-30 21:32:25 +0700

    Menambahkan folder semester - memasukkan semua file selama kuliah sem-1 kedalam folder SEM-1

---
+++ Kuliah/SEM-1/Kaderisasi/latihangit.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    cout << "Halo Semuanya Aku sedang belajar Git dan GitHub di pertemuan kaderisasi ke-7" << endl;
+    cout << "Semoga sukses selalu" << endl;
+
+    cin.get();
+    return 0;
+}
