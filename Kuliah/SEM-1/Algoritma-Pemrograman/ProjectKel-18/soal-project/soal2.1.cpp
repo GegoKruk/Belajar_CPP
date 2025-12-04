bfdfac6d87a698c3911017736c57faa6b80672ce
Gega Ramadhan <gegaramadhan17ok@gmail.com>
2025-12-04 09:14:25 +0700

    Menambahkan soal project 2.1 - project algo

---
+++ Kuliah/SEM-1/Algoritma-Pemrograman/ProjectKel-18/soal-project/soal2.1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+// Soal 2
+/*
+    Buatlah program (bahasa c++) untuk menginput suatu kalimat dan menghasilkan kalimat baru 
+	seperti contoh di bawah.
+
+	ketentuan : 
+	- input kalimat di program utama/main, 
+	- hasilnya dibuat dan ditampilkan di function,
+	- gunakan tipe data array (array of char), statement perulangan dan kondisional
+
+	Contoh tampilan :
+		kalimat = teknik informatika upn
+		hasil	= upn informatika teknik
+*/
+
+void proses(string kalimat){
+	string kata[100];
+	int jumlah_kata = 0;
+	string temp = "";
+	
+	for (int i = 0; i < kalimat.length(); i++) {
+		if (kalimat[i] == ' ') {
+			kata[jumlah_kata] = temp;
+			jumlah_kata++;
+			temp = "";
+		} else {
+			temp = temp + kalimat[i];
+		}
+	}
+		
+	kata[jumlah_kata] = temp;
+	jumlah_kata++;
+	
+	cout << "Hasil: ";
+	for (int i = jumlah_kata - 1; i >= 0; i--) {
+		cout << kata[i] << endl;
+
+		if (i > 0) {
+			cout << " ";
+		}
+	}
+}
+
+int main () {
+	string kalimat;
+	
+	cout << "Masukan Kalimat: "; 
+	getline(cin, kalimat);
+	
+	proses(kalimat);
+}
