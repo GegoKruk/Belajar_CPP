2384cefeb7e688f0cf0799979b333d92f0714533
Gega Ramadhan <gegaramadhan17ok@gmail.com>
2026-03-10 23:36:36 +0700

    Menambahkan File Tugas 1 (Struct, Rekursi, dan Searching) - Prak AlgoLanjut

---
+++ Kuliah/SEM-2/Prak-AlgoLanjut/Tugas/123250112_GegaRamadhan_Tugas1.cpp
@@ -0,0 +1,41 @@
+/*
+    TUGAS 1 - PRAKTIKUM ALGORITMA PEMROGRAMAN LANJUT
+
+    Log Catatan Kebugaran (Workout Log)
+
+    Aplikasi pencatat kebugaran membutuhkan program console untuk merekap riwayat latihan harian penggunanya.
+        ● Menu: 
+            Catat Sesi Workout, 
+            Riwayat Latihan, 
+            Cari Sesi, 
+            dan Exit.
+
+        ● Struct: 
+            ID Sesi, 
+            Jenis Latihan (contoh: Lari, Angkat Beban), 
+            Durasi (menit), 
+            dan Kalori Terbakar.
+
+        ● Rekursi: 
+            Buat fungsi rekursif hitungTotalKalori(array_sesi, n) yang menelusuri isi array of struct
+            menggunakan teknik divide and conquer atau pemanggilan rekursif mundur, untuk menjumlahkan 
+            seluruh Kalori Terbakar dari semua sesi latihan yang pernah dilakukan. 
+            Panggil fungsi ini di menu Riwayat Latihan.
+        
+        ● Searching: 
+            Cari sesi berdasarkan ID Sesi menggunakan Binary Search. 
+            (Pastikan data diurutkan berdasarkan ID terlebih dahulu).
+        
+        ● Format File: 
+            NIM_Nama_Tugas1.cpp
+*/  
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    
+
+    return 0;
+}
