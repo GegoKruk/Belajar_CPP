819f09ea6848b2d0cce67d9357a6b507c7851293
Gega Ramadhan <gegaramadhan17ok@gmail.com>
2026-05-22 21:41:17 +0700

    FEAT [SEM-2/Prak-AlgoLanjut]: Menambah helper function, menu utama, save file, read file (parsing), dan tambah data di 123250112_Latres_E.cpp > Latihan Responsi Prak. AlgoLanjut [WIP]

--- Kuliah/SEM-2/Prak-AlgoLanjut/Latihan-Responsi/123250112_Latres_E.cpp
+++ Kuliah/SEM-2/Prak-AlgoLanjut/Latihan-Responsi/123250112_Latres_E.cpp
@@ -1,27 +1,39 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <iomanip>
+
 using namespace std;
 
 // Struct
     struct AtributSihir {
-
+        int Teori;
+        int Praktek;
+        int Level_Fokus;
     };
 
     struct Siswa {
-
+        int ID;
+        string Nama;
+        string Asrama;
+        AtributSihir Statistik;
     };
 
 // Variabel Global
-    Siswa arraySiswa[100];
+    const int MAX_SISWA = 100;
+    Siswa arraySiswa[MAX_SISWA];
     int jumlah_siswa = 0;
 
 // Function Prototype 
-    // Function2
+    // Function FILE
         void initData();          // Dijalankan HANYA DI AWAL saat program pertama kali dibuka
         void saveAppendData();    // Digunakan khusus untuk menu Tambah Data (ios::app)
         void saveOverwriteData(); // Digunakan khusus untuk menu Hapus Data (overwrite)
 
+    // Function Searching 
         int Hitung_Bonus(int fokus);
+
+    // Function Rekursi
         int binarySearch(Siswa arr[], int kiri, int kanan, int id_cari);
 
     // Function Pointer dan Sorting (Quick Sort)
@@ -29,24 +41,53 @@ using namespace std;
         int partition(Siswa arr[], int low, int high);
         void quickSort(Siswa arr[], int low, int high);
 
-    // Function Menu 
+    // Function SubMenu 
         void tambahData();
         void lihatData();
         void hapusData();
         void cetakLaporan();
+    
+// Function Helper
+    void namaKelas(){
+        cout << "\n\t\t================================" << endl;
+        cout << "\t\t||    Nama : Gega Ramadhan    ||" << endl;
+        cout << "\t\t||    NIM  : 123250112        ||" << endl;
+        cout << "\t\t||    Plug : IF-E             ||" << endl;
+        cout << "\t\t================================\n\n" << endl;
+    }
 
+    void headerMenu(string judulMenu){
+        system("CLS");
+        namaKelas();
+        cout << "===============[ " << judulMenu << " ]===============" << endl;
+    }
+
+    void garis(){
+        cout << "--------------------------------------------------" << endl;
+    }
+
+    
+    void jeda() {
+        cout << "\n\t    Tekan Enter untuk lanjut...";
+        cin.get();
+    }
+
+
+// Function Main
 int main() {
     initData(); 
-        
     int pilihan;
+
     do {
-        cout << "\n=== SISTEM MANAJEMEN AKADEMI SIHIR ===\n";
-        cout << "[1] Tambah Data Siswa\n";
-        cout << "[2] Lihat Data Siswa\n";
-        cout << "[3] Hapus Data Siswa\n";
-        cout << "[4] Cetak Laporan Kelas Remedial\n";
-        cout << "[0] Keluar\n";
-        cout << "Pilih menu: ";
+        headerMenu("SISTEM MANAJEMEN AKADEMI SIHIR");
+
+        cout << " [1] Tambah Data Siswa\n";
+        cout << " [2] Lihat Data Siswa\n";
+        cout << " [3] Hapus Data Siswa\n";
+        cout << " [4] Cetak Laporan Kelas Remedial\n";
+        cout << " [0] Keluar\n\n";
+        cout << " Pilih menu: ";
+
         cin >> pilihan;
         cin.ignore();
         
@@ -64,62 +105,179 @@ int main() {
             cetakLaporan(); 
             break;
         case 0: 
-            cout << "Keluar dari program...\n"; 
+            cout << "\t    Program Selesai - Sampai Jumpa Lagi"; 
             break;
         default: 
-            cout << "Pilihan tidak valid! Silakan coba lagi.\n";
+            cout << "\t[!] Pilihan tidak valid - Silakan coba lagi...";
         }
     } while (pilihan != 0);
         
     return 0;
 }
 
+//Function2 FILE
 void initData() {
+    ifstream bacaFile("data_siswa.txt");
 
+    if(!bacaFile.is_open()){
+        jumlah_siswa = 0;
+        return;
+    }
+
+    char koma;
+    char karakter;
+
+    while(bacaFile >> arraySiswa[jumlah_siswa].ID >> koma){
+
+        string tempNama = "";
+        while(bacaFile.get(karakter) && karakter != ','){
+            tempNama += karakter;
+        }
+        arraySiswa[jumlah_siswa].Nama = tempNama;
+
+        string tempAsrama = "";
+        while(bacaFile.get(karakter) && karakter != ','){
+            tempNama += karakter;
+        }
+        arraySiswa[jumlah_siswa].Asrama = tempAsrama;
+
+        bacaFile >> arraySiswa[jumlah_siswa].Statistik.Teori >> koma;
+        bacaFile >> arraySiswa[jumlah_siswa].Statistik.Praktek >> koma;
+        bacaFile >> arraySiswa[jumlah_siswa].Statistik.Level_Fokus;
+
+        bacaFile.ignore();
+        jumlah_siswa++;
+    }
 }
 
 void saveAppendData() {
+    ofstream saveFile("data_siswa.txt", ios::app);
+    if(!saveFile.is_open()){
+        cout << "\n\t[!] File Gagal Dibuat/Ditulis" << endl;
+        return;
+    }
 
+    saveFile << arraySiswa[jumlah_siswa].ID << "," 
+            << arraySiswa[jumlah_siswa].Nama << ","
+            << arraySiswa[jumlah_siswa].Asrama << ","
+            << arraySiswa[jumlah_siswa].Statistik.Teori << ","
+            << arraySiswa[jumlah_siswa].Statistik.Praktek << ","
+            << arraySiswa[jumlah_siswa].Statistik.Level_Fokus << endl;
+
+    saveFile.close();
 }
 
 void saveOverwriteData() {
 
 }
 
+// Function Rekursi
 int Hitung_Bonus(int fokus) {
 
     return 0; 
 }
 
+// Function Searching
 int binarySearch(Siswa arr[], int kiri, int kanan, int id_cari) {
 
     return -1;
 }
 
+// Function Sorting
 void swapData(Siswa *a, Siswa *b) {
 
 }
 
 int partition(Siswa arr[], int low, int high) {
 
+    return 0;
 }
 
 void quickSort(Siswa arr[], int low, int high) {
 
 }
 
+// Function Sub-Menu
 void tambahData() {
+    
+    headerMenu("TAMBAH DATA SISWA");
 
+    // Validasi Slot Data Dulu
+    if(jumlah_siswa > MAX_SISWA){
+        cout << "\n\t[!] JUMLAH DATA SISWA MENCAPAI BATAS MAKSIMAL";
+        jeda();
+        return;
+    }
+
+    // Input Ke Array & Struct
+    cout << " Masukkan Data Siswa" << endl;
+    cout << " [1] ID          : ";
+    cin >> arraySiswa[jumlah_siswa].ID;
+    cin.ignore();
+
+    cout << " [2] Nama        : ";
+    getline(cin, arraySiswa[jumlah_siswa].Nama);
+    cin.ignore();
+
+    cout << " [3] Asrama      : ";
+    cin >> arraySiswa[jumlah_siswa].Asrama;
+    cin.ignore();
+
+    cout << " [4] Teori       : ";
+    cin >> arraySiswa[jumlah_siswa].Statistik.Teori;
+    cin.ignore();
+
+    cout << " [5] Praktek     : ";
+    cin >> arraySiswa[jumlah_siswa].Statistik.Praktek;
+    cin.ignore();
+
+    cout << " [6] Level Fokus : ";
+    cin >> arraySiswa[jumlah_siswa].Statistik.Level_Fokus;
+    cin.ignore();
+
+    saveAppendData();
+
+    cout << "\n\tDATA BERHASIL DITAMBAHKAN" << endl;
+
+    jumlah_siswa++;
+    jeda();
 }
 
 void lihatData() {
+    headerMenu("TAMPILKAN DATA SISWA");
+    if (jumlah_siswa == 0){
+        cout << "\n\t[!] DATA MASIH KOSONG - Input dulu datanya...";
+        jeda();
+        return;
+    }
 
+
+
+    jeda();
 }
 
 void hapusData() {
+    headerMenu("HAPUS DATA SISWA");
+    if (jumlah_siswa == 0){
+        cout << "\n\t[!] DATA MASIH KOSONG - Input dulu datanya...";
+        jeda();
+        return;
+    }
 
+
+
+    jeda();
 }
 
 void cetakLaporan() {
+    headerMenu("CETAK LAPORAN SISWA");
+    if (jumlah_siswa == 0){
+        cout << "\n\t[!] DATA MASIH KOSONG - Input dulu datanya...";
+        jeda();
+        return;
+    }
 
+
+
+    jeda();
 }
