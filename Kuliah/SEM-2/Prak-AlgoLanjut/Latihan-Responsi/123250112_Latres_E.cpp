0286c7f217ca14a2de6730fd22285975e6640e60
Gega Ramadhan <gegaramadhan17ok@gmail.com>
2026-05-24 12:15:57 +0700

    FEAT [SEM-2/Prak-AlgoLanjut]: Menambah kode di function lihatData(), binarySearch(), hitungBonus(), dan saveOverwriteData() > Latihan Responsi Prak. AlgoLanjut [WIP]

--- Kuliah/SEM-2/Prak-AlgoLanjut/Latihan-Responsi/123250112_Latres_E.cpp
+++ Kuliah/SEM-2/Prak-AlgoLanjut/Latihan-Responsi/123250112_Latres_E.cpp
@@ -74,13 +74,14 @@ using namespace std;
 
 // Function Main
 int main() {
+    system("color 03");
     initData(); 
     int pilihan;
 
     do {
         headerMenu("SISTEM MANAJEMEN AKADEMI SIHIR");
 
-        cout << " [1] Tambah Data Siswa\n";
+        cout << "\n [1] Tambah Data Siswa\n";
         cout << " [2] Lihat Data Siswa\n";
         cout << " [3] Hapus Data Siswa\n";
         cout << " [4] Cetak Laporan Kelas Remedial\n";
@@ -167,18 +168,53 @@ void saveAppendData() {
 }
 
 void saveOverwriteData() {
+    ofstream saveTimpa("data_siswa.txt");
 
+    if(!saveTimpa.is_open()){
+        cout << "\n\t[!] File Gagal Dibuat/Ditulis" << endl;
+        return;
+    }
+
+    for (int i = 0; i < jumlah_siswa; i++){
+        saveTimpa << arraySiswa[i].ID << ","
+                    << arraySiswa[i].Nama << ","
+                    << arraySiswa[i].Asrama << ","
+                    << arraySiswa[i].Statistik.Teori << ","
+                    << arraySiswa[i].Statistik.Praktek << ","
+                    << arraySiswa[i].Statistik.Level_Fokus << endl;
+    }
+    
+    saveTimpa.close();
 }
 
 // Function Rekursi
 int Hitung_Bonus(int fokus) {
-
-    return 0; 
+    if (fokus == 0){
+        return 0;
+    } 
+    
+    return (fokus * 10) + Hitung_Bonus(fokus - 1);
 }
 
 // Function Searching
 int binarySearch(Siswa arr[], int kiri, int kanan, int id_cari) {
 
+    while(kiri <= kanan){
+
+        int mid = (kiri + kanan) / 2;
+        
+        if(arr[mid].ID == id_cari){
+
+            return mid;
+
+        } else if (id_cari < arr[mid].ID){
+            kanan = mid - 1; 
+        } else if (id_cari > arr[mid].ID){
+            kiri = mid + 1; 
+        }
+    }
+    
+    cout << "\n\t[!] ID SISWA TIDAK DITEMUKAN" << endl;
     return -1;
 }
 
@@ -226,7 +262,7 @@ void tambahData(){
     }
 
     // Input Ke Array & Struct
-    cout << " Masukkan Data Siswa" << endl;
+    cout << "\n Masukkan Data Siswa" << endl;
     cout << " [1] ID          : ";
     cin >> arraySiswa[jumlah_siswa].ID;
     cin.ignore();
@@ -270,18 +306,67 @@ void lihatData(){
     // Manggil Quick Sort
     quickSort(arraySiswa, 0, jumlah_siswa-1);
 
+    // Perulangan untuk tampilan data dan perhitungan Mana
+    for (int i = 0; i < jumlah_siswa; i++){
+
+        int hasilHitungFokus = Hitung_Bonus(arraySiswa[i].Statistik.Level_Fokus);
+        int totalMana = arraySiswa[i].Statistik.Teori + arraySiswa[i].Statistik.Praktek + hasilHitungFokus;
+
+        cout << "\n [DATA SISWA " << i+1 << "]---------------------------------------" << endl;
+        cout << " [1] ID            : " << arraySiswa[i].ID << endl;
+        cout << " [2] Nama          : " << arraySiswa[i].Nama << endl;
+        cout << " [3] Asrama        : " << arraySiswa[i].Asrama << endl;
+        cout << " [4] Nilai Teori   : " << arraySiswa[i].Statistik.Teori << endl;
+        cout << " [5] Nilai Praktek : " << arraySiswa[i].Statistik.Praktek << endl;
+        cout << " [6] Level Fokus   : " << arraySiswa[i].Statistik.Level_Fokus << endl;
+        cout << " [7] Total Mana    : " << totalMana << endl;
+    }
+
+    cout << " -----------------------------------------------------" << endl;
+    cout << "\n\t[!] SEMUA DATA TELAH DITAMPILKAN" << endl;
+    
     jeda();
 }
 
 void hapusData() {
-    headerMenu("HAPUS DATA SISWA");
-    if (jumlah_siswa == 0){
-        cout << "\n\t[!] DATA MASIH KOSONG - Input dulu datanya...";
-        jeda();
-        return;
-    }
+    char pilihan;
 
+    do {
+        headerMenu("HAPUS DATA SISWA");
+        if (jumlah_siswa == 0){
+            cout << "\n\t[!] DATA MASIH KOSONG - Input dulu datanya...";
+            jeda();
+            return;
+        }
 
+        // Manggil Quick Sort
+        quickSort(arraySiswa, 0, jumlah_siswa-1);
+
+        cout << "\n Masukkan ID Siswa : ";
+        int idDicari = 0;
+        cin >> idDicari;
+        cin.ignore();
+    
+        // Panggil Binary Search
+        int posisiKetemu = binarySearch(arraySiswa, 0, jumlah_siswa - 1, idDicari);
+    
+        if(posisiKetemu != -1){
+            for (int i = posisiKetemu; i < jumlah_siswa; i++){
+                arraySiswa[i] = arraySiswa[i+1];
+            }
+
+            jumlah_siswa--;
+        }
+    
+        saveOverwriteData();
+    
+        // Menampilkan Pilihan
+        cout << "\n\t[SUKSES] DATA ID \""<< idDicari <<"\" TELAH DIHAPUS" << endl;
+        cout << "\n Hapus Data Lainnya? [Y/N] : ";
+        cin >> pilihan;
+        cin.ignore();
+
+    } while (pilihan == 'Y' || pilihan == 'y');
 
     jeda();
 }
